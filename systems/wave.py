0cbbe491e2335e7facd23bebf6ac8a1710b6b1ee
lhbeen01 <lhbeen01@gmail.com>
2026-05-28 04:28:02 +0900

    난이도 조정

--- systems/wave.py
+++ systems/wave.py
@@ -2,103 +2,10 @@ from models.enemy import create_enemy
 from settings import WAVE_CLEAR_BONUS, COLOR_GHOST_RED, COLOR_GHOST_PINK, COLOR_GHOST_CYAN, COLOR_GHOST_ORANGE
 
 WAVES = [
-    [('ghost_normal', 10), ('boss_rocket', 1)],
-    [('rocket_normal', 15), ('boss_pacman', 1)],
-    [('pacman_normal', 10), ('boss_ghost_sequence', 1)], # 유령 보스 4종류 큐 예약
-    [('rocket_normal', 10), ('pacman_normal', 10), ('boss_pacman', 1), ('boss_rocket', 1)],
-    [('boss_ghost_sequence', 1), ('boss_pacman', 1)],
-]
-
-class WaveManager:
-    def __init__(self, economy=None, stage_id=1):
-        self.economy = economy
-        self.stage_id = stage_id
-        self.current_wave = 0
-        self.queue = []
-        self.spawn_timer = 0
-        self.spawn_interval = 0.75
-        self.boss_spawn_interval = 0.4 # 유령 보스 연속 스폰 간격
-        self.active = False
-        self.all_clear = False
-        self.waiting_for_clear = False
-        self.scale = 1.0
-
-    def start_next_wave(self, path, effect_manager=None):
-        if self.current_wave >= len(WAVES): 
-            self.all_clear = True
-            return []
-        
-        self.current_wave += 1
-        self.scale = 1 + (self.current_wave - 1) * 0.32 + (self.stage_id - 1) * 0.18
-        self.queue = []
-        
-        for kind, count in WAVES[self.current_wave - 1]:
-            if kind == 'boss_ghost_sequence':
-                # 보스 유령 시퀀스는 4개의 색상을 연속으로 큐에 삽입
-                colors = [COLOR_GHOST_RED, COLOR_GHOST_PINK, COLOR_GHOST_CYAN, COLOR_GHOST_ORANGE]
-                for c in colors:
-                    self.queue.append(('boss_ghost', c))
-            else:
-                self.queue += [(kind, None)] * count
-                
-        self.active = True
-        self.waiting_for_clear = True
-        self.spawn_timer = 0
-
-    def update(self, dt, path, effect_manager=None):
-        made = []
-        if not self.active: 
-            return made
-            
-        self.spawn_timer -= dt
-        
-        while self.queue and self.spawn_timer <= 0:
-            kind, ghost_color = self.queue.pop(0)
-            
-            # 💡 팩토리 함수를 통해 알맞은 자식 클래스 인스턴스 생성!
-            new_enemy = create_enemy(kind, path, self.scale, ghost_color)
-            made.append(new_enemy)
-            
-            # 다음 적도 유령 보스면 스폰 간격 단축
-            if kind == 'boss_ghost' and self.queue and self.queue[0][0] == 'boss_ghost':
-                self.spawn_timer += self.boss_spawn_interval
-            else:
-                self.spawn_timer += self.spawn_interval
-                
-        if not self.queue: 
-            self.active = False
-        return made
-    @property
-    def spawner_done(self): 
-        return not self.active and not self.queue
-    def on_wave_enemies_cleared(self):
-        if getattr(self,'just_loaded',False):
-            return
-        if self.waiting_for_clear:
-            if self.economy: self.economy.earn(WAVE_CLEAR_BONUS + self.current_wave*25)
-            self.waiting_for_clear=False
-            if self.current_wave >= len(WAVES): 
-                self.all_clear=True
-            else: self.start_next_wave(None)
-    def to_dict(self): 
-        return {'current_wave':self.current_wave, 'all_clear':self.all_clear}
-    
-    def restore(self, data): 
-        self.current_wave=data.get('current_wave',0)
-        self.all_clear=data.get('all_clear',False)
-        self.active=False
-        self.queue=[]
-        self.waiting_for_clear=False
-        self.just_loaded=True
-
-    from models.enemy import create_enemy
-from settings import WAVE_CLEAR_BONUS, COLOR_GHOST_RED, COLOR_GHOST_PINK, COLOR_GHOST_CYAN, COLOR_GHOST_ORANGE
-
-WAVES = [
-    [('ghost_normal', 10), ('boss_rocket', 1)],
-    [('rocket_normal', 15), ('boss_pacman', 1)],
-    [('pacman_normal', 10), ('boss_ghost_sequence', 1)], # 유령 보스 4종류 큐 예약
-    [('rocket_normal', 10), ('pacman_normal', 10), ('boss_pacman', 1), ('boss_rocket', 1)],
+    [('rocket_normal', 10), ('boss_rocket', 1)],
+    [('pacman_normal', 15), ('boss_pacman', 1)],
+    [('ghost_normal', 10), ('boss_ghost_sequence', 1)], # 유령 보스 4종류 큐 예약
+    [('pacman_normal', 10), ('pacman_normal', 10), ('boss_pacman', 1), ('boss_rocket', 1)],
     [('boss_ghost_sequence', 1), ('boss_pacman', 1)],
 ]
 
